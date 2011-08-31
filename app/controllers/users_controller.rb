32dbb83ded3a68349a76ead30b4e33a2fbe6d890
Camilo Arthur <camilo.arthur@gmail.com>
2011-08-30 23:37:33 -0300

    Revert "Simplify users controller"
    This reverts commit 55768e2da2de6d96cf3fdb2e0570abce49c601ea.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,11 @@ class UsersController < ApplicationController
   # GET /users.xml
   def index
     @users = User.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @users }
+    end
   end
 
   # GET /users/1
